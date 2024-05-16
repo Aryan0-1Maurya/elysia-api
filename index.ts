37b96f62e34b8f01e62e185b5b45034e9fec4670
Aryan Maurya <136960772+Aryan0-1Maurya@users.noreply.github.com>
2024-05-16 12:35:54 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,28 @@
+import { Elysia } from 'elysia'
+//
+import { userRoutes } from './routes'
+import { error, logger } from './middlewares'
+import { connectDB } from './config'
+
+// Create Elysia instance
+const app = new Elysia()
+
+// Config MongoDB
+connectDB()
+
+// Middlewares
+app.use(logger())
+app.use(error())
+
+// Root Routes
+app.get('/', () => 'Welcome to our API')
+
+// User Routes [api/v1/users]
+app.use(userRoutes)
+
+// Start the server
+app.listen(Bun.env.PORT || 9000)
+
+console.log(
+  `🚀 Server is running at ${app.server?.hostname}:${app.server?.port}`
+)
